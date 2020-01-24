dfe4cf5d9eaa54cc95fbe5b7c13d722f5ddbd3d9
youssef103 <youssefmoti@yahoo.com>
2020-01-24 01:59:59 +0100

    put the title div inout side of card content

--- src/features/card/components/Card.component.tsx
+++ src/features/card/components/Card.component.tsx
@@ -20,48 +20,50 @@ export const CardComponent: React.FC<ICardProps> = ({
     return <Error errors={errors} bg />;
 
   return (
-    <Row col={cards && cards.length === 2 ? 2 : 1}>
-      {compensationPeriod && (
+    <>
+      {cards && compensationPeriod && (
         <Title md={5} color={colors.primary}>
           {compensationPeriod}
         </Title>
       )}
-      {cards &&
-        cards.map((cardData: ICard, index: number) => (
-          <StyledCard key={index}>
-            <StyledCard.Body>
-              <StyledCard.ListItem>
-                <span>{cardText.max10PBB}</span>
-                <span>{cardData.max10PBB}</span>
-              </StyledCard.ListItem>
+      <Row col={cards && cards.length === 2 ? 2 : 1}>
+        {cards &&
+          cards.map((cardData: ICard, index: number) => (
+            <StyledCard key={index}>
+              <StyledCard.Body>
+                <StyledCard.ListItem>
+                  <span>{cardText.max10PBB}</span>
+                  <span>{cardData.max10PBB}</span>
+                </StyledCard.ListItem>
 
-              <StyledCard.ListItem>
-                <span>{cardText.max15PBB}</span>
-                <span>{cardData.max15PBB}</span>
-              </StyledCard.ListItem>
+                <StyledCard.ListItem>
+                  <span>{cardText.max15PBB}</span>
+                  <span>{cardData.max15PBB}</span>
+                </StyledCard.ListItem>
 
-              <StyledCard.ListItem>
-                <span>{cardText.excessFixedSalary}</span>
-                <span>{cardData.excessFixedSalary}</span>
-              </StyledCard.ListItem>
+                <StyledCard.ListItem>
+                  <span>{cardText.excessFixedSalary}</span>
+                  <span>{cardData.excessFixedSalary}</span>
+                </StyledCard.ListItem>
 
-              <StyledCard.ListItem>
-                <span>{cardText.parentalSalaryUpto10PBB}</span>
-                <span>{cardData.parentalSalaryUpto10PBB}</span>
-              </StyledCard.ListItem>
+                <StyledCard.ListItem>
+                  <span>{cardText.parentalSalaryUpto10PBB}</span>
+                  <span>{cardData.parentalSalaryUpto10PBB}</span>
+                </StyledCard.ListItem>
 
-              <StyledCard.ListItem>
-                <span>{cardText.parentalSalaryAbove10PBB}</span>
-                <span>{cardData.parentalSalaryAbove10PBB}</span>
-              </StyledCard.ListItem>
-            </StyledCard.Body>
+                <StyledCard.ListItem>
+                  <span>{cardText.parentalSalaryAbove10PBB}</span>
+                  <span>{cardData.parentalSalaryAbove10PBB}</span>
+                </StyledCard.ListItem>
+              </StyledCard.Body>
 
-            <StyledCard.Footer>
-              <span>{cardText.total}</span>
-              <span>{cardData.total} kr</span>
-            </StyledCard.Footer>
-          </StyledCard>
-        ))}
-    </Row>
+              <StyledCard.Footer>
+                <span>{cardText.total}</span>
+                <span>{cardData.total} kr</span>
+              </StyledCard.Footer>
+            </StyledCard>
+          ))}
+      </Row>
+    </>
   );
 };
