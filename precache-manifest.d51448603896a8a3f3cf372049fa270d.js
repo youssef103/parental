45695f77ec35ff90673dc839564782aa5e2b2960
youssef103 <youssefmoti@yahoo.com>
2021-03-18 17:13:12 +0000

    Updates

---
+++ precache-manifest.d51448603896a8a3f3cf372049fa270d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9e392b9204a17d85b32c9bcb38dbed",
+    "url": "/parental/index.html"
+  },
+  {
+    "revision": "401082f2471b40232bb3",
+    "url": "/parental/static/css/2.7be5aaf4.chunk.css"
+  },
+  {
+    "revision": "401082f2471b40232bb3",
+    "url": "/parental/static/js/2.89297a0d.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/parental/static/js/2.89297a0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4db8d28cb09b58b9e553",
+    "url": "/parental/static/js/main.18dda611.chunk.js"
+  },
+  {
+    "revision": "bae318f5fdbee0d8c260",
+    "url": "/parental/static/js/runtime-main.ed9ef8b3.js"
+  }
+]);
